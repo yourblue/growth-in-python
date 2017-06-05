4c87fa1ac923575ee782ea537e4557e62820f289
qiuhy <qiuhaiyanglian@163.com>
2017-06-05 15:51:10 +0800

    delete some useless files

--- django/django_mysql/mysql/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Blogpost(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    author = models.CharField(max_length=100, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
-    body = models.TextField()
-    posted = models.DateField(db_index=True, auto_now_add=True)
